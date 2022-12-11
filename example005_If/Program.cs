3c800cf028eafda4338b3affe4e4750a1c50d681
mihail58 <nafanai@96yandex.ru>
2022-12-11 12:53:01 +0300

    Написал программу с условиями

---
+++ example005_If/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше имя:");
+string username = Console.ReadLine();
+
+if( username.ToLower() == "Маша" )
+  {
+     Console.WriteLine("Ура, это же Маша!");
+  }
+ else
+  {
+     Console.Write("Привет, ");
+     Console.WriteLine(username);
+  }
