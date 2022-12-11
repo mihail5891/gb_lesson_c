22b2e0c5395ccf602d7a99896680b7fa9125d265
mihail58 <nafanai@96yandex.ru>
2022-12-11 12:40:47 +0300

    Написал программу с сложением рандомных чисел

--- example003_Sum/Program.cs
+++ example003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 3;
-int namberB = 5;
+﻿int numberA = new Random().Next(1,10); // Случайное число от 1 до 10
+Console.WriteLine(numberA);
+int namberB = new Random().Next(1,10); // Случайное число от 1 до 10
+Console.WriteLine(namberB);
 Console.WriteLine(numberA + namberB);
